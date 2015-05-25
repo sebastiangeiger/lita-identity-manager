dd55fdbc58cc2dd3e5105a8c6444af5bf4991bc9
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-05-25 18:57:07 -0400

    Routing works

--- lib/lita/handlers/identity_manager.rb
+++ lib/lita/handlers/identity_manager.rb
@@ -1,6 +1,9 @@
 module Lita
   module Handlers
     class IdentityManager < Handler
+      route /.+ is .+ on github$/, :new_github_identity,
+        command: true,
+        help: { "CHAT_USER is GITHUB_USER on github" => "Will link the chat user to the specified github username" }
     end
 
     Lita.register_handler(IdentityManager)
--- spec/lita/handlers/identity_manager_spec.rb
+++ spec/lita/handlers/identity_manager_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Handlers::IdentityManager, lita_handler: true do
+
+  describe '#new_github_identity' do
+    it { is_expected.to route_command("@sebastian is @sebastiangeiger on github").to(:new_github_identity) }
+  end
 end
