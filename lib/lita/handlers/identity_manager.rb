1c8b071809e044f0699acecd3f60eea48f0d7501
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-05-25 19:49:10 -0400

    Confirmation message after adding github identity

--- lib/lita/handlers/identity_manager.rb
+++ lib/lita/handlers/identity_manager.rb
@@ -1,9 +1,18 @@
 module Lita
   module Handlers
     class IdentityManager < Handler
-      route /.+ is .+ on github$/, :new_github_identity,
+      NEW_GITHUB_IDENTITY_REGEX = /(.+) is (.+) on github$/i
+
+      route NEW_GITHUB_IDENTITY_REGEX, :new_github_identity,
         command: true,
         help: { "CHAT_USER is GITHUB_USER on github" => "Will link the chat user to the specified github username" }
+
+      def new_github_identity(response)
+        match_data = response.message.body.match(NEW_GITHUB_IDENTITY_REGEX)
+        chat_name = match_data[1]
+        github_name = match_data[2]
+        response.reply("Linked #{chat_name} to #{github_name} on github")
+      end
     end
 
     Lita.register_handler(IdentityManager)
--- spec/lita/handlers/identity_manager_spec.rb
+++ spec/lita/handlers/identity_manager_spec.rb
@@ -3,6 +3,11 @@ require "spec_helper"
 describe Lita::Handlers::IdentityManager, lita_handler: true do
 
   describe '#new_github_identity' do
-    it { is_expected.to route_command("@sebastian is @sebastiangeiger on github").to(:new_github_identity) }
+    it { is_expected.to route_command("Lita: @sebastian is @sebastiangeiger on github").to(:new_github_identity) }
+
+    it "replies with a confirmation message" do
+      send_message("Lita: @sebastian is @sebastiangeiger on github")
+      expect(replies.last).to eql "Linked @sebastian to @sebastiangeiger on github"
+    end
   end
 end
