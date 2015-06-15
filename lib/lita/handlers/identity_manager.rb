dc70a02ba430b765da67d408652177b90a7b702c
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-06-14 21:08:53 -0400

    Renaming response parameter to incoming_message

--- lib/lita/handlers/identity_manager.rb
+++ lib/lita/handlers/identity_manager.rb
@@ -8,22 +8,22 @@ module Lita
         command: true,
         help: { "I am GITHUB_USER on github" => "Will link your chat identity to the specified github username" }
 
-      def new_github_identity(response)
-        chat_name = response.user.id
-        match_data = response.message.body.match(NEW_GITHUB_IDENTITY_REGEX)
+      def new_github_identity(incoming_message)
+        chat_name = incoming_message.user.id
+        match_data = incoming_message.message.body.match(NEW_GITHUB_IDENTITY_REGEX)
         github_name = match_data[1]
         redis.set("github:#{chat_name}", github_name)
         reply = "Linked #{chat_name} to #{github_name} on github"
-        log.debug(%Q{"#{response.message.body}" by "#{response.user}" -> "#{reply}"})
-        response.reply(reply)
+        log.debug(%Q{"#{incoming_message.message.body}" by "#{incoming_message.user}" -> "#{reply}"})
+        incoming_message.reply(reply)
       end
 
       route GET_GITHUB_IDENTITY_REGEX, :get_github_identity,
         command: true,
         help: { "Who is CHAT_USER on github?" => "Returns the github username for CHAT_USER" }
 
-      def get_github_identity(response)
-        match_data = response.message.body.match(GET_GITHUB_IDENTITY_REGEX)
+      def get_github_identity(incoming_message)
+        match_data = incoming_message.message.body.match(GET_GITHUB_IDENTITY_REGEX)
         chat_name = match_data[1]
         github_name = redis.get("github:#{chat_name}")
         reply = if github_name
@@ -31,8 +31,8 @@ module Lita
                 else
                   "I don't know about #{chat_name} on github"
                 end
-        log.debug(%Q{"#{response.message.body}" by "#{response.user}" -> "#{reply}"})
-        response.reply(reply)
+        log.debug(%Q{"#{incoming_message.message.body}" by "#{incoming_message.user}" -> "#{reply}"})
+        incoming_message.reply(reply)
       end
     end
 
